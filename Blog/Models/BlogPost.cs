2eda7f447447a19080a8570776548987ae2cab57
CEPHARD KIPKEMBOI <58904994+cephard@users.noreply.github.com>
2025-06-06 00:49:40 +0100

    first commit

---
+++ Blog/Models/BlogPost.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Blog.Models
+{
+    public class BlogPost
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public required string ImageURL { get; set; }
+        public required string[] Tags { get; set; }
+        public required string Content { get; set; }
+    }
+}
---
+++ Blog/Program.cs
@@ -0,0 +1,109 @@
+﻿using MongoDB.Driver;
+using Blog.Models;
+using Microsoft.VisualBasic;
+using Microsoft.Extensions.Hosting;
+
+namespace Blog
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddAuthorization();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            // Add CORS policy for Vite frontend
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowViteDevSever", policy =>
+                {
+                    policy.WithOrigins("http://localhost:5173")
+                          .AllowAnyMethod()
+                          .AllowAnyHeader();
+                });
+            });
+
+            var app = builder.Build();
+
+            // MongoDB connection setup
+            var config = app.Configuration;
+            var mongoConnectionString = config.GetSection("BlogDatabaseSettings")["ConnectionString"];
+
+            var client = new MongoClient(mongoConnectionString);
+            var database = client.GetDatabase("blog");
+            var blogCollection = database.GetCollection<BlogPost>("blogpost");
+
+            //end points
+            //posting a new blogpost into mongodb
+            app.MapPost("/blogposts", async (BlogPost newBlogPost) =>
+            {
+                try
+                {
+                    await blogCollection.InsertOneAsync(newBlogPost);
+                    return Results.Created($"/blogposts/{newBlogPost.Id}", newBlogPost);
+                }
+                catch (Exception ex)
+                {
+                    return Results.Problem("Failed to post blog : " + ex.Message);
+
+                }
+            }
+            ).WithName("CreateBlog")
+             .WithTags("MongoDb");
+
+            //updating the blogpost
+            //replaces the entire parts of the blog can be expensive
+            app.MapPut("/blogposts/{id}",async(string id, BlogPost blogToUpdate) => 
+            {
+                var modified = await blogCollection.ReplaceOneAsync(x => x.Id==id, blogToUpdate);
+                return modified.ModifiedCount > 0 ? Results.Ok("Post Updated Suceesfully") : Results.NotFound();
+            }
+            ).WithName("UpdateBlog")
+             .WithTags("MongoDb");
+
+            //getting the blogposts
+            // Getting the blogposts
+            app.MapGet("/blogposts", async () => {
+                var posts = await blogCollection.Find(_ => true).ToListAsync();
+                return Results.Ok(posts);
+            })
+            .WithName("GetAllBlogPosts")
+            .WithTags("MongoDb");
+
+            //getting the blogpost by id 
+            app.MapGet("/blogposts/{id}", async (string id) =>
+            {
+                var post = await blogCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+                return post is not null ? Results.Ok<BlogPost>(post) : Results.NotFound();
+            }).WithName("BlogById")
+            .WithTags("MongoDb");
+
+            //deleting the blogpost
+            app.MapDelete("/blogposts/{id}", async (string id)=>
+            {
+                var postToDelete = await blogCollection.DeleteOneAsync(x => x.Id == id);
+                return postToDelete is not null ? Results.Ok("Deleted!") : Results.NotFound();
+            }).WithName("BlogToDelete")
+            .WithTags("MongoDb");
+
+            // Enable Swagger in dev mode
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseCors("AllowViteDevSever");
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+
+            app.Run();
+            //return Task.CompletedTask;.
+
+        }
+    }
+}
