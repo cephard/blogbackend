ffed4ece076ddd5c969ec63291f0b489d387858d
CEPHARD KIPKEMBOI <58904994+cephard@users.noreply.github.com>
2025-06-11 02:28:37 +0100

    testing endpoint

--- Blog/obj/Debug/net8.0/Blog.AssemblyInfo.cs
+++ Blog/obj/Debug/net8.0/Blog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c9bed64174b7c33e1ce7fd238593b5fd1dd5c78")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+496a3522d3cae00f55115b284d695d9a410267ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
